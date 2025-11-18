8847a33275a224253454454fef17969674b27aec
MichaelGurevich <michaelgur273@gmailc.com>
2025-11-18 20:33:37 +0200

    implemented get solution accuracy function

--- main.py
+++ main.py
@@ -2,6 +2,25 @@ import json
 import time
 
 
+def get_solution_accuracy(label: list, solution: list) -> float:
+    list_len = len(label)
+
+    if list_len == 0:
+        return 1.0
+
+    if list_len != len(solution):
+        return 0
+    
+    predicted_correct = 0
+    for i in range(list_len):
+        if label[i] == solution[i]:
+            predicted_correct += 1
+    
+    return predicted_correct / list_len
+
+
+
+
 def solve_cloze(input, candidates, corpus, left_only):
     # todo: implement this function
     print(f'starting to solve the cloze {input} with {candidates} using {corpus}')
