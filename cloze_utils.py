dd0b67fba0b5ba80535e3e25b4c9b2b417dcc5cd
MichaelGurevich <michaelgur273@gmailc.com>
2025-11-23 23:15:50 +0200

    add new implementation of getting context

--- cloze_utils.py
+++ cloze_utils.py
@@ -1,5 +1,9 @@
-
+import string
 import re
+
+SENTENCE_START_SYMBOL = "<s>"
+SENTENCE_END_SYMBOL = "</s>"
+CLOZE_BLANK_SYMBOL = "__________"
 
 #function that finds the indices of the blanks in a string
 # returns a list of tuples (starting index, end index).
@@ -58,3 +62,65 @@ def get_context(text, blank_indices, n=1, left = False):
 def get_all_contexts(text, n=2, left=False):
     blanks = find_blanks(text)
     return [get_context(text, b, n=n, left=left) for b in blanks]
+
+
+
+def get_contexts(cloze_filename:str) -> list:
+    """
+     Extract left and right context words (up to 2 on each side) surrounding cloze blanks.
+
+     Args:
+         cloze_filename: Path to file with sentences containing cloze blanks
+
+     Returns:
+         List of dicts with "left_context" and "right_context" for each blank
+
+     Note: Assumes cloze blanks are not the only word in a sentence.
+     """
+
+    punctuation_without_underscore = string.punctuation.replace('_', '')
+    translator = str.maketrans('', '', punctuation_without_underscore)
+
+    contexts_list = []
+
+
+    with open(cloze_filename, 'r', encoding='utf-8') as fin:
+        for line in fin:
+
+            cleaned_line = line.translate(translator).split()
+
+            # add start and end sentence symbols
+            cleaned_line.insert(0, SENTENCE_START_SYMBOL)
+            cleaned_line.append(SENTENCE_END_SYMBOL)
+
+            for i in range (1, len(cleaned_line)-1, 1):
+                if cleaned_line[i] != CLOZE_BLANK_SYMBOL:
+                    continue
+
+                context = {
+                    "left_context": [],
+                    "right_context": []
+                }
+
+                prev_word = cleaned_line[i-1]
+                next_word = cleaned_line[i+1]
+
+                if prev_word == SENTENCE_START_SYMBOL: # candidate is the 1st word in the sentence
+                    context["left_context"].append(SENTENCE_START_SYMBOL)
+                    context["right_context"].append(next_word)
+                    context["right_context"].append(cleaned_line[i+2])
+                elif next_word == SENTENCE_END_SYMBOL:
+                    context["right_context"].append(SENTENCE_END_SYMBOL)
+                    context["left_context"].append(cleaned_line[i - 2])
+                    context["left_context"].append(prev_word)
+                else:
+                    context["right_context"].append(next_word)
+                    context["right_context"].append(cleaned_line[i+2])
+
+                    context["left_context"].append(cleaned_line[i - 2])
+                    context["left_context"].append(prev_word)
+
+
+                contexts_list.append(context)
+
+    return contexts_list
